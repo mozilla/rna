9a5778a0c9bd7c40e02fa8f3f957ee70c4535ec3
Josh Mize <jgmize@gmail.com>
2014-05-02 07:12:12 +0000

    Add /release/:id/notes/ api + use in admin
    Add /auth_token/ url + view
    Replace NoteInline admin with ajax note table using api
    
    Fix bug 1004015: Updating the Note.releases m2m relation from the
                     release page in the admin should update the
                     Note.modified field
    Fix bug 979380:  Hide Note ID for bug
    Fix bug 974703:  Make it possible to add notes on the fly
                     from release view
    Fix bug 979390:  Add Edit link to edit notes from release detail
    Fix bug 979946:  RNA API: filter notes by release

--- rna/urls.py
+++ rna/urls.py
@@ -1,7 +1,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
+from django.conf.urls import patterns, url
 from rest_framework import routers
 
 from . import views
@@ -11,4 +11,7 @@ router = routers.DefaultRouter()
 router.register('notes', views.NoteViewSet)
 router.register('releases', views.ReleaseViewSet)
 
-urlpatterns = router.urls
+urlpatterns = router.urls + patterns(
+    '',
+    url(r'^releases/(?P<pk>\d+)/notes/$', views.NestedNoteView.as_view()),
+    url(r'^auth_token/$', views.auth_token))
--- rna/views.py
+++ rna/views.py
@@ -2,9 +2,26 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from itertools import chain
+import json
+
+from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
 from rest_framework.viewsets import ModelViewSet
 
 from . import models
+
+
+def auth_token(request):
+    if request.user.is_active and request.user.is_staff:
+        token, created = Token.objects.get_or_create(user=request.user)
+        return HttpResponse(
+            content=json.dumps({'token': token.key}),
+            content_type='application/json')
+    else:
+        return HttpResponseForbidden()
 
 
 class NoteViewSet(ModelViewSet):
@@ -13,3 +30,11 @@ class NoteViewSet(ModelViewSet):
 
 class ReleaseViewSet(ModelViewSet):
     model = models.Release
+
+
+class NestedNoteView(generics.ListAPIView):
+    model = models.Note
+
+    def get_queryset(self):
+        release = get_object_or_404(models.Release, pk=self.kwargs.get('pk'))
+        return chain(*release.notes())
