47589c7852b762f31ef069a9ead446e13088af14
Josh Mize <jgmize@gmail.com>
2014-05-20 01:10:55 -0500

    Add Note.is_public for bug 1005998

---
+++ rna/migrations/0004_auto__add_field_note_is_public.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Note.is_public'
+        db.add_column('rna_note', 'is_public',
+                      self.gf('django.db.models.fields.BooleanField')(default=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Note.is_public'
+        db.delete_column('rna_note', 'is_public')
+
+
+    models = {
+        'rna.note': {
+            'Meta': {'object_name': 'Note'},
+            'bug': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'fixed_in_release': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'fixed_note_set'", 'null': 'True', 'to': "orm['rna.Release']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_known_issue': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'blank': 'True'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'releases': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['rna.Release']", 'symmetrical': 'False', 'blank': 'True'}),
+            'sort_num': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'tag': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        'rna.release': {
+            'Meta': {'ordering': "('product', '-version', 'channel')", 'unique_together': "(('product', 'version'),)", 'object_name': 'Release'},
+            'bug_list': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'bug_search_url': ('django.db.models.fields.CharField', [], {'max_length': '2000', 'blank': 'True'}),
+            'channel': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_public': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True', 'blank': 'True'}),
+            'product': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'release_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'system_requirements': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'text': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['rna']
