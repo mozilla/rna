045f04cf6727e4174dac211f084c93a514fe271b
Paul McLanahan <pmac@mozilla.com>
2016-07-26 11:47:39 -0400

    Remove itertools import

--- rna/views.py
+++ rna/views.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from itertools import chain
 import json
 
 from django.http import HttpResponse, HttpResponseForbidden
